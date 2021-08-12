40b28d462670f746896586519d44b2911e7b4515
Ben Griffith <griffith.ben@gmail.com>
2021-08-11 23:25:46 -0500

    layout adjustments

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Col, Typography, Avatar, Space, Button } from "antd";
+import { Col, Typography, Avatar, Space, Button, Divider } from "antd";
 import {
   GithubFilled,
   LinkedinFilled,
@@ -8,15 +8,26 @@ import {
 } from "@ant-design/icons";
 import { Fade, Slide } from "react-awesome-reveal";
 import WhiteBox from "../../modules/WhiteBox";
-import { HeaderRow, PaddedRow } from "./styles";
+import { HeaderRow, PaddedLightRow, PaddedDarkRow, SkillTag } from "./styles";
 
 const { Title } = Typography;
+
+const green = "#59a947";
+const brown = "#736357";
+const orange = "#f26522";
+const fuschia = "#ec008c";
+const blue = "#4775a9";
 
 function Home() {
   return (
     <div>
       <HeaderRow>
-        <Col span={12} style={{ textAlign: "right" }}>
+        <Col
+          xs={{ span: 24, pull: 5 }}
+          sm={{ span: 24, pull: 7 }}
+          md={{ span: 12, pull: 0 }}
+          style={{ textAlign: "right" }}
+        >
           <Fade>
             <Avatar
               src="/img/me.jpg"
@@ -28,7 +39,12 @@ function Home() {
             />
           </Fade>
         </Col>
-        <Col span={11} offset={1} style={{ textAlign: "left", paddingTop: 26 }}>
+        <Col
+          xs={{ span: 24, push: 6 }}
+          sm={{ span: 24, push: 8 }}
+          md={{ span: 11, push: 0, offset: 1 }}
+          style={{ textAlign: "left", paddingTop: 26 }}
+        >
           <Fade>
             <Title
               className="bebasNeueRegular"
@@ -81,21 +97,8 @@ function Home() {
             </Space>
           </Fade>
         </Col>
-        <Col span={23} offset={1} style={{ textAlign: "center" }}>
-            <br/><br/><br/>
-            <Title
-              className="bebasNeueRegular"
-              style={{ fontSize: "3em", marginBottom: 0, color: "white" }}
-            >
-              Contributions
-            </Title>
-          <img
-            src="http://ghchart.rshah.org/griffithben"
-            alt="Ben Griffith Github Chart"
-          ></img>
-        </Col>
       </HeaderRow>
-      <PaddedRow>
+      <PaddedLightRow>
         <Col
           md={{ span: 16, pull: 4, push: 4 }}
           lg={{ span: 12, pull: 0, push: 0 }}
@@ -118,11 +121,28 @@ function Home() {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
+            <p>
+              <Space wrap={true} size={6}>
+                <SkillTag color={green}>PHP 7</SkillTag>
+                <SkillTag color={green}>Javascript</SkillTag>
+                <SkillTag color={brown}>Mysql</SkillTag>
+                <SkillTag color={orange}>Laravel</SkillTag>
+                <SkillTag color={orange}>React</SkillTag>
+                <SkillTag color={orange}>Cypress</SkillTag>
+                <SkillTag color={fuschia}>Docker</SkillTag>
+                <SkillTag color={fuschia}>GIT</SkillTag>
+                <SkillTag color={fuschia}>Codeship</SkillTag>
+                <SkillTag color={fuschia}>Docker</SkillTag>
+                <SkillTag color={fuschia}>AWS</SkillTag>
+                <SkillTag color={blue}>Photoshop</SkillTag>
+              </Space>
+            </p>
+            <Divider />
             <Button>Go to Site</Button>
           </WhiteBox>
         </Col>
-      </PaddedRow>
-      <PaddedRow>
+      </PaddedLightRow>
+      <PaddedLightRow>
         <Col
           md={{ span: 16, pull: 4, push: 4 }}
           lg={{ span: 12, pull: 0, push: 0 }}
@@ -145,11 +165,32 @@ function Home() {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
+            <p>
+              <Space wrap={true} size={6}>
+                <SkillTag color={green}>PHP 7</SkillTag>
+                <SkillTag color={green}>PHP 5</SkillTag>
+                <SkillTag color={green}>Javascript</SkillTag>
+                <SkillTag color={brown}>Mysql</SkillTag>
+                <SkillTag color={brown}>Postgres</SkillTag>
+                <SkillTag color={orange}>Laravel</SkillTag>
+                <SkillTag color={orange}>React</SkillTag>
+                <SkillTag color={orange}>Cypress</SkillTag>
+                <SkillTag color={orange}>Backbone</SkillTag>
+                <SkillTag color={orange}>Zend</SkillTag>
+                <SkillTag color={fuschia}>Docker</SkillTag>
+                <SkillTag color={fuschia}>GIT</SkillTag>
+                <SkillTag color={fuschia}>Codeship</SkillTag>
+                <SkillTag color={fuschia}>Docker</SkillTag>
+                <SkillTag color={fuschia}>AWS</SkillTag>
+                <SkillTag color={blue}>Photoshop</SkillTag>
+              </Space>
+            </p>
+            <Divider />
             <Button>Go to Site</Button>
           </WhiteBox>
         </Col>
-      </PaddedRow>
-      <PaddedRow>
+      </PaddedLightRow>
+      <PaddedLightRow>
         <Col
           md={{ span: 16, pull: 4, push: 4 }}
           lg={{ span: 12, pull: 0, push: 0 }}
@@ -172,10 +213,32 @@ function Home() {
               enim ad minim veniam, quis nostrud exercitation ullamco laboris
               nisi ut aliquip ex ea commodo consequat.
             </p>
+            <p>
+              <Space wrap={true} size={6}>
+                <SkillTag color={green}>Javascript</SkillTag>
+                <SkillTag color={orange}>Hexo</SkillTag>
+                <SkillTag color={fuschia}>Heroku</SkillTag>
+              </Space>
+            </p>
+            <Divider />
             <Button>Go to Site</Button>
           </WhiteBox>
         </Col>
-      </PaddedRow>
+      </PaddedLightRow>
+      <PaddedDarkRow>
+        <Col span={23} offset={1} style={{ textAlign: "center" }}>
+          <Title
+            className="bebasNeueRegular"
+            style={{ fontSize: "3em", marginBottom: 0, color: "white" }}
+          >
+            Github Contributions
+          </Title>
+          <img
+            src="http://ghchart.rshah.org/griffithben"
+            alt="Ben Griffith Github Chart"
+          ></img>
+        </Col>
+      </PaddedDarkRow>
     </div>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,12 +1,24 @@
 import styled from "styled-components";
-import { Row } from "antd";
+import { Row, Tag } from "antd";
 
 export const HeaderRow = styled(Row)`
     padding: 6em 0;
     background-image: url(/img/bg.png);
+    margin: 0em -24px 2em -24px;
+`;
+
+export const PaddedLightRow = styled(Row)`
+    padding-top: 2em;
+    padding-bottom: 2em;
+`;
+
+export const PaddedDarkRow = styled(Row)`
+    padding-top: 2em;
+    padding-bottom: 2em;
+    background-image: url(/img/bg.png);
     margin: 0px -24px;
 `;
 
-export const PaddedRow = styled(Row)`
-    padding-top: 4em;
+export const SkillTag = styled(Tag)`
+    margin-right: 0px;
 `;
